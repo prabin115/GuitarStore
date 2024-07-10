4704317d271b19936e2dadd57567d81f9f015a4e
prabin <prabinxwork@gmail.com>
2024-07-10 14:55:10 +0530

    Lots of changes made for making the app production ready FRONTEND created docker image for react and implemented nginx server. created docker compose to launch both images together. --- BACKEND created docker image for springboot backend and a docker compose file to run mysql and springboot together

--- Frontend/guitar-store/src/components/Home/Home.jsx
+++ Frontend/guitar-store/src/components/Home/Home.jsx
@@ -6,19 +6,9 @@ const Home = () => {
    
     return (
         <>
-            <Carousel/>
-            
-            {/* <div className='text-white text-4xl font-bold p-5 mt-5 ml-9'>
-                <h1>WHAT'S NEW</h1>
+            <div className='relative overflow-hidden h-screen'>
+                <Carousel/>
             </div>
-
-            <GuitarCard/>
-
-            <div className='text-white text-4xl font-bold p-5 mt-5 ml-9'>
-                <h1>SPOTLIGHT STORIES</h1>
-            </div>
-            
-            <Stories/> */}
             
             <SubFooter/>
         </>
